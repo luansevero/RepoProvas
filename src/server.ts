1269093e978e4877fe5e9e2206d7b47c512924ee
Luan Severo <luansevero@icloud.com>
2022-09-14 19:10:40 -0300

    [Feat] Adding ErrorHandlerMiddleware

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routers";
 
+import { errorHandler } from "./middlewares/errorHandlerMiddleware";
+
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(json());
 app.use(router);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT : number = Number(process.env.PORT) || 5000;
 app.listen(PORT, () => {console.log(`Server listening on ${PORT}`)});
