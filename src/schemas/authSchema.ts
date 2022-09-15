cdb9591b14d1b55513554e6f1f4774aeff7a9de2
Luan Severo <luansevero@icloud.com>
2022-09-15 15:05:53 -0300

    [Fix/Feat/Test] Fix:Controller file name & Feat: AuthRouter and link to main router & Test: Trying spread operator in schema

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,7 +1,11 @@
 import Joi from "joi";
 
-export const auth = Joi.object({
+export const signin = Joi.object({
     email : Joi.string().email({ tlds : { allow : false }}).required(),
-    password: Joi.string().min(8).max(40).required,
+    password: Joi.string().min(8).max(40).required
+})
+
+export const signup = Joi.object({
+    ...signin,
     confirmPassword: Joi.ref("password")
 })
