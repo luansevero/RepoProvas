6246d039af3978a2d606fb2588dbdc7e54b07927
Luan Severo <luansevero@icloud.com>
2022-09-15 13:44:05 -0300

    [Fix] AuthRepository

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,5 +1,20 @@
-import { prisma }
+import { prisma } from "../config/database"
+import { TAuthData } from "../types/authTypes"
 
 export async function findByEmail( email : string ){
-    return await prisma.users.find
-}
+    return await prisma.user.findUnique({
+        where: { email }
+    })
+};
+
+export async function findById( id : number ){
+    return await prisma.user.findUnique({
+        where: { id }
+    })
+};
+
+export async function insert(authData : TAuthData){
+    await prisma.user.create({
+        data: authData
+    })
+};
