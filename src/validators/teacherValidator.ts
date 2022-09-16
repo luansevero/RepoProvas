36cec5a899c685cc32803583a1ff2e62ed937665
Luan Severo <luansevero@icloud.com>
2022-09-15 23:13:57 -0300

    [Feat] Teacher Validator

---
+++ src/validators/teacherValidator.ts
@@ -0,0 +1,7 @@
+import { ErrorInfo } from "../middlewares/errorHandlerMiddleware";
+import * as teacherRepository from "../repositories/teacherRepository";
+
+export async function findTeacher(name : string){
+    const teacherId = await teacherRepository.findByName(name);
+    if(!teacherId) throw new ErrorInfo("error_not_found", "That teacher doesn't exist");
+};
